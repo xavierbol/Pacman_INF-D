f508b94a03877ddd696420598facc90faa31c5e9
Xavier Bol <xavi.120595@gmail.com>
2020-03-09 17:17:19 +0100

    Add unit test to improve test coverage

---
+++ src/test/java/pacman_infd/ScorePanelTest.java
@@ -0,0 +1,20 @@
+package pacman_infd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScorePanelTest {
+    private ScorePanel scorePanel;
+
+    @Before
+    public void setUp() {
+        scorePanel = new ScorePanel();
+    }
+
+    @Test
+    public void testLifeWhenPacmanLoseALife() {
+        int lives = scorePanel.getLives();
+        scorePanel.loseLife();
+        assert lives - 1 == scorePanel.getLives();
+    }
+}
